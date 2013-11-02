46830eb48f125a0cf9dafc23cc5ffbe849235953
Attila Csala <attilacsala@gmail.com>
2013-11-02 12:29:50 +0100

    Initial Commit

---
+++ demodice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demodice
+//
+//  Created by Attila Csala on 11/2/13.
+//  Copyright (c) 2013 Attila Csala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
